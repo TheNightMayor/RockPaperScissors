7d8de5862af673be4ed0810eb100229458bd787f
Lawrence <lawrencekeeganleblanc@gmail.com>
2021-11-20 07:45:02 -0600

    2021-11-20-07:44 rock paper scissors completed

---
+++ script.js
@@ -0,0 +1,42 @@
+function computerPlay() {
+    let compChoice = ['rock','paper','scissors']
+       return compChoice[Math.floor(Math.random() * compChoice.length)]
+}
+
+function playerPlay() {
+    let playerInput = prompt("rock, paper, or scissors? Best three out of five wins!");
+    playerInput = playerInput.toLowerCase();
+    return playerInput;
+}
+
+let losses = 0;
+let wins = 0;
+
+function playRound() {
+    let playerSelection = playerPlay();
+    let computerSelection = computerPlay(); 
+    return  ((playerSelection === 'rock' && computerSelection === 'paper' ) ||
+        (playerSelection === 'paper' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'rock')) 
+        ? `${computerSelection} beats ${playerSelection}! You've lost this round! wins: ${wins}, losses: ${++losses}`
+        :((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')) 
+        ? `${playerSelection} beats ${computerSelection}! You've won this round! wins: ${++wins}, losses: ${losses}`
+        : (playerSelection === computerSelection)
+        ? `it's a tie! wins: ${wins}, losses: ${losses}`
+        : `Something went wrong! Try Again` 
+}
+
+function matchResults() {
+    return (wins === 3) 
+    ? 'You have won this match!'
+    : (losses === 3) 
+    ? 'You have lost this match!'
+    : 'Let\'s go again!';
+}
+
+for (; (wins < 3 && losses < 3);) {
+    console.log(playRound());
+    console.log(matchResults())
+}
