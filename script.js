bb072b3107be36ceb35a1508f9e004083ff82eaa
Lawrence LeBlanc <lawrencekeeganleblanc@gmail.com>
2022-07-24 16:43:34 -0500

    did the thing

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
 function computerPlay() {
-    let compChoice = ['rock','paper','scissors']
-       return compChoice[Math.floor(Math.random() * compChoice.length)]
+  let compChoice = ["rock", "paper", "scissors"];
+  return compChoice[Math.floor(Math.random() * compChoice.length)];
 }
-
-function playerPlay() {
-    let playerInput = prompt("rock, paper, or scissors? Best three out of five wins!");
-    playerInput = playerInput.toLowerCase();
-    return playerInput;
-}
-
 let losses = 0;
 let wins = 0;
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let score = document.getElementById("score");
+let game = document.getElementById("game");
 
-function playRound() {
-    let playerSelection = playerPlay();
-    let computerSelection = computerPlay(); 
-    return  ((playerSelection === 'rock' && computerSelection === 'paper' ) ||
-        (playerSelection === 'paper' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'rock')) 
-        ? `${computerSelection} beats ${playerSelection}! You've lost this round! wins: ${wins}, losses: ${++losses}`
-        :((playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')) 
-        ? `${playerSelection} beats ${computerSelection}! You've won this round! wins: ${++wins}, losses: ${losses}`
-        : (playerSelection === computerSelection)
-        ? `it's a tie! wins: ${wins}, losses: ${losses}`
-        : `Something went wrong! Try Again` 
+function updateScore() {
+  score.innerHTML =
+    wins === 5
+      ? `You have won this match!`
+      : losses === 5
+      ? `You have lost this match!`
+      : `Let's go again!`;
 }
 
-function matchResults() {
-    return (wins === 3) 
-    ? `You have won this match!` 
-    : (losses === 3) 
-    ? `You have lost this match!`
-    : `Let's go again!`;
+rock.addEventListener("click", playRound);
+paper.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
+
+function playRound(e) {
+  let playerSelection = e.target.id;
+  console.log(playerSelection);
+  let computerSelection = computerPlay();
+  console.log(computerSelection);
+  (playerSelection === "rock" && computerSelection === "paper") ||
+  (playerSelection === "paper" && computerSelection === "scissors") ||
+  (playerSelection === "scissors" && computerSelection === "rock")
+    ? (game.innerHTML = `${computerSelection} beats ${playerSelection}! You've lost this round! wins: ${wins}, losses: ${++losses}`)
+    : (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ? (game.innerHTML = `${playerSelection} beats ${computerSelection}! You've won this round! wins: ${++wins}, losses: ${losses}`)
+    : playerSelection === computerSelection
+    ? (game.innerHTML = `it's a tie! wins: ${wins}, losses: ${losses}`)
+    : (game.innerHTML = `Something went wrong! Try Again`);
+  updateScore();
 }
 
-for (; (wins < 3 && losses < 3);) {
-    console.log(playRound());
-    console.log(matchResults());
-    }
+// for (; (wins < 3 && losses < 3);) {
+//     console.log(playRound());
+//     console.log(matchResults());
+//     }
